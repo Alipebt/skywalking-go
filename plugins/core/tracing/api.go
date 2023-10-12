60686049066ffb517a1b608926c4e9cdf3dc539f
Alipebt <1160756683@qq.com>
2023-10-12 11:25:17 +0800

    is ok?
    Signed-off-by: Alipebt <1160756683@qq.com>

--- plugins/core/tracing/api.go
+++ plugins/core/tracing/api.go
@@ -173,6 +173,7 @@ func SetCorrelationContextValue(key, val string) {
 	}
 }
 
+// SetCorrelationConfig sets the value of the CorrelationConfig
 func SetCorrelationConfig(maxKeyCount, maxValueSize int) {
 	op := operator.GetOperator()
 	if op != nil {
