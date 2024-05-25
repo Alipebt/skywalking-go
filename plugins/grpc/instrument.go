0950649044b87bd1bc42f45badc4665abec0a801
Alipebt <1160756683@qq.com>
2024-05-25 23:15:21 +0800

    funlen
    Signed-off-by: Alipebt <1160756683@qq.com>

--- plugins/grpc/instrument.go
+++ plugins/grpc/instrument.go
@@ -46,8 +46,8 @@ func (i *Instrument) VersionChecker(version string) bool {
 	return true
 }
 
+// nolint:funlen
 func (i *Instrument) Points() []*instrument.Point {
-	//skywalking:nocopy
 	return []*instrument.Point{
 		{
 			PackagePath: "",
